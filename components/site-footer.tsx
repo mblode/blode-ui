523ac4c2ffb8234de1b31d1ee5ab356ccb3191f4
Matthew Blode <matthew@linktr.ee>
2026-02-18 20:51:06 +1100

    fix

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -1,8 +1,8 @@
 export function SiteFooter() {
   return (
-    <footer className="relative border-border border-t py-6 md:py-0">
-      <div className="container flex flex-col items-center justify-between gap-4 md:h-16 md:flex-row">
-        <p className="text-center text-muted-foreground text-sm leading-loose md:text-left">
+    <footer className="relative border-border border-t py-6">
+      <div className="container flex flex-col items-center justify-between gap-4 text-center">
+        <p className="text-center text-muted-foreground text-sm leading-loose">
           Created by{" "}
           <a
             className="font-medium underline underline-offset-4"
@@ -13,7 +13,6 @@ export function SiteFooter() {
           >
             Matthew Blode
           </a>
-          .
         </p>
       </div>
     </footer>
